2fbfb761acc82225e0fd515e9ceb280d8b0ff898
Oles <oblazhivsky@gmail.com>
2015-06-15 22:24:02 +0300

    Add angular-fullstack

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('researchApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
