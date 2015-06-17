174b28ed80a1a7d61b36af038cc684793b9f5533
Oles <oblazhivsky@gmail.com>
2015-06-17 10:07:12 +0300

    Add carousel and tags to Home page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,28 @@
 'use strict';
 
 angular.module('researchApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+  .controller('MainCtrl', function ($scope) {
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
+    // Carousel
+    $scope.myInterval = 5000;
+    var slides = $scope.slides = [];
+    $scope.addSlide = function() {
+      var newWidth = 1170 + slides.length;
+      slides.push({
+        image: 'http://placekitten.com/' + newWidth + '/500'
+      });
     };
+    for (var i=0; i<4; i++) {
+      $scope.addSlide();
+    }
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    // Tags
+    $scope.tags = [
+      {name: 'Tag 1', link: '/'},
+      {name: 'Tag 2', link: '/'},
+      {name: 'Tag 3', link: '/'},
+      {name: 'Tag 4', link: '/'}
+    ];
 
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
+
   });
