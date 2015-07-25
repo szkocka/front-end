2f07e0827894393c5e05fbeb8e638a1484dc8c1b
Oles <oblazhivsky@gmail.com>
2015-07-25 15:49:46 +0300

    remove unnecessary Thing endpoint

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,6 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/projects', require('./api/project'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
