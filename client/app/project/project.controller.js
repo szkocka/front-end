f8ef85ccae9bbd8a9a2c4d04f49014804fe1f0c1
Anton Iatsenko <anton.yacenko@gmail.com>
2015-09-05 14:55:41 +0300

    forums list/forum messages

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -2,30 +2,36 @@
 
 angular.module('researchApp')
   .controller('ProjectCtrl', function ($scope, $routeParams, $http) {
-    var mainForum;
+    $scope.forums = [];
+    $scope.activeForum = null;
 
     $http.get(API_URL + 'researches/' + $routeParams.id).success(function(project) {
       $scope.project = project;
     });
 
     $http.get(API_URL + 'researches/' + $routeParams.id + '/forums').success(function(forums) {
-      mainForum = forums.forums[0];
-      if( mainForum && mainForum._id ) {
-        $http.get(API_URL + 'researches/forums/' + mainForum._id).success(function(forum) {
-          $scope.forum = forum;
-        });
-      }
+      $scope.forums = forums.forums;
     });
+
+    $scope.setActiveForum = function(forum){
+      $scope.activeForum = forum;
+      $http.get(API_URL + 'researches/forums/' + forum._id).success(function(forum) {
+        $scope.activeForum.messages = forum.messages;
+      });
+    };
+    $scope.disableActiveForum = function(){
+      $scope.activeForum = null;
+    };
 
     $scope.newMessage = null;
     $scope.postMessage = function(text){
       $scope.newMessage = null;
-      $http.post(API_URL + 'researches/forums/' + mainForum._id, {
+      $http.post(API_URL + 'researches/forums/' + $scope.activeForum._id, {
         'message': text
       }).success(function(response){
-        $scope.forum.messages.push({
+        $scope.activeForum.messages.push({
           message: text,
-          createBy: 'You',
+          createdBy: {name:'You'},
           created: new Date()
         })
       })
