7c9579f66c7c484192a184ea7e4a32e484d5d4d9
Anton Iatsenko <anton.yacenko@gmail.com>
2015-09-05 15:43:22 +0300

    new topics

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -1,20 +1,27 @@
 'use strict';
 
 angular.module('researchApp')
-  .controller('ProjectCtrl', function ($scope, $routeParams, $http) {
+  .controller('ProjectCtrl', function ($scope, $routeParams, $http, Auth) {
     $scope.forums = [];
     $scope.activeForum = null;
+    $scope.forumsAccessError = true;
 
     $http.get(API_URL + 'researches/' + $routeParams.id).success(function(project) {
       $scope.project = project;
+      var user = Auth.getCurrentUser();
+      if( user && user._id == project.supervisor.id ){
+        $scope.canCreateForum = true;
+      }
     });
 
-    $http.get(API_URL + 'researches/' + $routeParams.id + '/forums').success(function(forums) {
-      $scope.forumsAccessError = false;
-      $scope.forums = forums.forums;
-    }).error(function(){
-      $scope.forumsAccessError = true;
-    });
+    function getForums(){
+      $http.get(API_URL + 'researches/' + $routeParams.id + '/forums').success(function(forums) {
+        $scope.forumsAccessError = false;
+        $scope.forums = forums.forums;
+      }).error(function(){
+        $scope.forumsAccessError = true;
+      });
+    }
 
     $scope.setActiveForum = function(forum){
       $scope.activeForum = forum;
@@ -24,7 +31,17 @@ angular.module('researchApp')
     };
     $scope.disableActiveForum = function(){
       $scope.activeForum = null;
+      getForums();
     };
+
+    $scope.createForum = function(topic){
+      $http.post(API_URL + 'researches/' + $routeParams.id + '/forums', {
+        subject: topic
+      }).success(function(forum){
+        forum._id = forum.forum_id;
+        $scope.setActiveForum(forum);
+      });
+    }
 
     $scope.newMessage = null;
     $scope.postMessage = function(text){
@@ -39,4 +56,9 @@ angular.module('researchApp')
         })
       })
     }
+
+    Auth.isLoggedInAsync(function(login){
+      if(login)
+        getForums();
+    });
   });
