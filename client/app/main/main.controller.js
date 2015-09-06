190b90575ebe15a115927ed5292677fafd3f3f55
Anton Iatsenko <anton.yacenko@gmail.com>
2015-09-06 12:58:15 +0300

    tag filter on main page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,12 +3,29 @@
 angular.module('researchApp')
   .controller('MainCtrl', function ($scope, $http) {
 
+    var allProjects;
+
     $scope.carouselInterval = 5000;
     $scope.projectsList = [];
 
     $http.get(API_URL + 'researches').success(function(projectsList) {
-      console.log(projectsList);
       $scope.projectsList = projectsList.researches;
+      allProjects = _.clone($scope.projectsList);
+      $scope.tags = _.chain(projectsList.researches)
+        .map(function(r){
+          return r.tags;
+        })
+        .flatten()
+        .uniq()
+        .sortBy()
+        .value()
     });
+
+    $scope.activateTag = function(tag){
+      $scope.projectsList = _.filter(allProjects, function(project){
+        return _.indexOf(project.tags, tag) > -1;
+      });
+      $scope.activeTag = tag;
+    }
 
   });
