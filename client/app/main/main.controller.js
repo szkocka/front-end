290ce49aabac69b5ce52c34559dbe254beeb66b1
anton <anton@MacBook-Pro-Kateryna.local>
2015-09-12 15:07:48 +0300

    mathjax directive

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -36,16 +36,3 @@ angular.module('researchApp')
     }
 
   });
-
-        angular.module('researchApp').directive("mathjaxBind", function() {
-            return {
-                restrict: "A",
-                controller: ["$scope", "$element", "$attrs",
-                    function($scope, $element, $attrs) {
-                        $scope.$watch($attrs.mathjaxBind, function(texExpression) {
-                            $element.html(texExpression);
-                            MathJax.Hub.Queue(["Typeset", MathJax.Hub, $element[0]]);
-                        });
-                    }]
-            };
-        });
