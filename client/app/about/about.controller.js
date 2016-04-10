edaf6cb37da939d9e7208b46953dc7067cb8bae4
Svitlana <svitlana.sikora@gmail.com>
2016-04-10 18:27:06 +0300

    Edit About page

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -1,6 +1,40 @@
 'use strict';
 
 angular.module('researchApp')
-  .controller('AboutCtrl', function ($scope) {
-    
+  .controller('AboutCtrl', function ($scope, $http, Auth) {
+  	$scope.aboutProject = {
+  		currentDescription: '',
+  		newDescription: ''
+  	};
+  	$scope.errorMsg = '';
+  	$scope.successMsg = '';
+  	$scope.showEditButton = (Auth.getCurrentUser().role === 'admin');
+  	$scope.showEditableTexarea = false;
+
+  	$http.get(API_URL + 'about').success(function(about) {
+      	$scope.aboutProject.currentDescription = about;
+      	$scope.aboutProject.newDescription = about;
+    }).error(function(error){});
+
+    $scope.updateAbout = function() {
+    	$http.put(API_URL + 'about', {about: $scope.aboutProject.description})
+    		.success(function(about){
+          		$scope.aboutProject.currentDescription = about;
+          		$scope.showEditableTexarea = false;
+          		$scope.successMsg = 'Information was saved successfully';
+        }).error(function(error){
+        		$scope.errorMsg = 'Error: Information was not updated';
+          		$scope.showEditableTexarea = false;
+        });
+    };
+
+    $scope.edit = function() {
+    	$scope.errorMsg = '';
+    	$scope.successMsg = '';
+    	$scope.showEditableTexarea = true;
+    };
+
+    $scope.cancel = function() {
+    	$scope.showEditableTexarea = false;
+    };
   });
