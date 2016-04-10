27c5f40b5bd45c57d4e8ac0a6df6e59f6c46521d
Svitlana <svitlana.sikora@gmail.com>
2016-04-10 23:39:56 +0300

    Edit About page

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -11,15 +11,15 @@ angular.module('researchApp')
   	$scope.showEditButton = (Auth.getCurrentUser().role === 'admin');
   	$scope.showEditableTexarea = false;
 
-  	$http.get(API_URL + 'about').success(function(about) {
-      	$scope.aboutProject.currentDescription = about;
-      	$scope.aboutProject.newDescription = about;
+  	$http.get(API_URL + 'pages/about').success(function(about) {
+      	$scope.aboutProject.currentDescription = about.content;
+      	$scope.aboutProject.newDescription = about.content;
     }).error(function(error){});
 
     $scope.updateAbout = function() {
-    	$http.put(API_URL + 'about', {about: $scope.aboutProject.description})
+    	$http.post(API_URL + 'pages/about', {content: $scope.aboutProject.newDescription})
     		.success(function(about){
-          		$scope.aboutProject.currentDescription = about;
+          		$scope.aboutProject.currentDescription = about.content;
           		$scope.showEditableTexarea = false;
           		$scope.successMsg = 'Information was saved successfully';
         }).error(function(error){
