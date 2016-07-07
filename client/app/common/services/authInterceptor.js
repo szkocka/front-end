68c0a8a2d7c0a061283a7d8ded1cad1058af2093
Svitlana <sshabaranska@n-ix.com>
2016-07-07 11:05:19 +0300

    Fix

--- client/app/common/services/authInterceptor.js
+++ client/app/common/services/authInterceptor.js
@@ -6,25 +6,24 @@ define(['angular'], function(angular) {
         return {
             // Add authorization token to headers
             request: function (config) {
-              config.headers = config.headers || {};
+                config.headers = config.headers || {};
 
-              if (config.url && config.url.indexOf('http') !== -1 && $cookieStore.get('token')) {
-                config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
-              }
-              return config;
+                if (config.url && config.url.indexOf('http') !== -1 && $cookieStore.get('token')) {
+                    config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+                }
+                return config;
             },
 
             // Intercept 401s and redirect you to login
             responseError: function(response) {
-              if(response.status === 401) {
-                $location.path('/login');
-                // remove any stale tokens
-                $cookieStore.remove('token');
-                return $q.reject(response);
-              }
-              else {
-                return $q.reject(response);
-              }
+                if(response.status === 401) {
+                    $location.path('/login');
+                    // remove any stale tokens
+                    $cookieStore.remove('token');
+                    return $q.reject(response);
+                } else {
+                    return $q.reject(response);
+                }
             }
         };
     }
